d456fb1ec9e14d5d46a8bdb24918344df0032046
Sage Skaff <skaffsage@gmail.com>
2023-01-14 10:23:34 -0800

    Add get_recipes_spec.rb and test for recipes by country api endpoint

---
+++ spec/requests/get_recipes_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Get Recipes' do
+    it 'returns recipes by country' do
+        country = "Thailand"
+
+        get "/api/v1/recipes?country=#{country}"
+
+        expect(response).to be successful
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response).to have_key(:data)
+        expect(parsed_response[:data]).to be_a(Array)
+        expect(parsed_response[:data].first).to have_key(:id)
+        expect(parsed_response[:data].first).to have_key(:type)
+        expect(parsed_response[:data].first).to have_key(:attributes)
+        expect(parsed_response[:data].first[:attributes]).to have_key(:title)
+        expect(parsed_response[:data].first[:attributes]).to have_key(:url)
+        expect(parsed_response[:data].first[:attributes]).to have_key(:country)
+        expect(parsed_response[:data].first[:attributes]).to have_key(:image)
+    end
+end
