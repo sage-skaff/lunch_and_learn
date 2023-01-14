61146271fbfbeb309a7b040e1fd2a1c10738ca42
Sage Skaff <skaffsage@gmail.com>
2023-01-14 10:10:22 -0800

    Update get_all_countries test

--- spec/services/rest_countries_service_spec.rb
+++ spec/services/rest_countries_service_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe RestCountriesService do
     it 'gets a list of all countries' do
         response = RestCountriesService.get_all_countries
 
-        expect(response).to be_a(Hash)
+        expect(response).to be_a(Array)
+        expect(response.first).to be_a(Hash)
+        expect(response.first).to have_key(:name)
+        expect(response.first).to have_key(:subregion)
+        expect(response.first[:name]).to have_key(:common)
     end
 end
