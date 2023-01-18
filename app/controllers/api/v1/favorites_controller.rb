461f0c366ad2d54021663131db4271502e80f65e
Sage Skaff <skaffsage@gmail.com>
2023-01-18 13:38:25 -0800

    Refactor favorites#create

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -15,12 +15,10 @@ module Api
       def create
         if User.find_by(api_key: params[:api_key]) != nil
           user = User.find_by(api_key: params[:api_key])
-          favorite = Favorite.create(favorite_params)
-          favorite.user_id = user.id
-          favorite.save
-          render json: { "success": 'Favorite added successfully' }, status: 201
+          user.favorites.create(favorite_params)
+          render json: { success: 'Favorite added successfully' }, status: 201
         else
-          render json: { "error": 'Unable to add favorite' }, status: :bad_request
+          render json: { error: 'Unable to add favorite' }, status: :bad_request
         end
       end
 
